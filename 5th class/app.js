f194248111b6a2b96848223f0f35bb36d9fe9d64
Syed Farhan <80678813+syedfarhan1224@users.noreply.github.com>
2024-02-29 17:47:39 +0500

    5th class assignment done

---
+++ 5th class/app.js
@@ -0,0 +1,90 @@
+// Q no 1
+var x = 25;
+var y = 7;
+var z = x % y;
+console.log(z);
+
+// Q no 2
+var a = 15 + 10;
+var b = 4 - 2;
+
+var c = a * b;
+console.log(c);
+
+// Q no 3
+var c = 12;
+var d = 3;
+var z = c ** d;
+console.log(z);
+
+// Q no 4
+var e = 10;
+var f = 3;
+var z = (e += f * 2);
+console.log(z);
+
+// Q no 5
+var g = 20 + 5;
+var h = 4;
+var z = g / h;
+console.log(z);
+
+// Q no 6
+var i = 8 - 2;
+var j = 3;
+var z = i * j;
+console.log(z);
+
+// Q no 7
+var k = 18;
+var l = 5;
+var z = k - l + 2;
+console.log(z);
+
+// Q no 8
+var m = 7;
+var n = 2;
+var z = m ** n;
+console.log(z);
+
+// Q no 9
+var o = 13;
+var p = 4;
+var z = o % (p + 2);
+console.log(z);
+
+// Q no 10
+var q = 16 / 2;
+var r = 3;
+var z = q * r;
+console.log(z);
+
+// Q no 11
+var s = 25 - 1;
+var t = 3;
+var z = s / t;
+console.log(z);
+
+// Q no 12
+var u = 11;
+var v = 3 + 2;
+var z = u - v;
+console.log(z);
+
+// Q no 13
+var w = 22;
+var x = 5;
+var z = w + x ** 2;
+console.log(z);
+
+// Q no 14
+var y = 19;
+var z = 4;
+var x = y % (z + 1);
+console.log(x);
+
+// Q no 15
+var a = 10;
+var b = 3;
+var z = a * (b - 2);
+console.log(z);
