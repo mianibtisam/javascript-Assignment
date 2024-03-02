990eb05a99f67a0aedad50895c72802cbfb44f73
Syed Farhan <80678813+syedfarhan1224@users.noreply.github.com>
2024-03-02 18:05:51 +0500

    6th & 7th chapter completed

---
+++ 6th class/app.js
@@ -0,0 +1,55 @@
+// Q=1
+var count = 8;
+var newCount = count++;
+
+console.log(count);
+console.log(newCount);
+// Q=2
+var x = 15;
+var newX = --x;
+
+console.log(x);
+console.log(newX);
+// Q=3
+var num = 25;
+var newNum = num--;
+
+console.log(num);
+console.log(newNum);
+// Q=4
+var ten = 10;
+var newTen = ++ten;
+
+console.log(ten);
+console.log(newTen);
+// Q=5
+var fiv = 5;
+var newfiv = fiv++;
+
+console.log(fiv);
+console.log(newfiv);
+// Q=6
+var quantity = 12;
+var newQuantity = --quantity;
+
+console.log(newQuantity);
+// Q=7
+var counter = 18;
+var newCounter = counter++;
+
+console.log(newCounter);
+// Q=8
+var score = 30;
+var newScore = score--;
+
+console.log(newScore);
+// Q=9
+var value = 20;
+var newValue = ++value;
+
+console.log(newValue);
+// Q=10
+var eight = 8;
+var newEight = eight--;
+
+console.log(newEight);
---
+++ 7th class/app.js
@@ -0,0 +1,49 @@
+// Q=1
+var firstName = "Ibtisam";
+var fullName = firstName + "Tariq";
+console.log(fullName);
+// Q=2
+
+var city = "new";
+var cName = city + "York";
+console.log(cName);
+
+// Q=3
+var fruit = "apple";
+var dish = fruit + "pie";
+console.log(dish);
+
+// Q=4
+var color = "black";
+var animal = color + "bird";
+console.log(animal);
+
+// Q=5
+var a = "JavaScript";
+var b = "is fun";
+console.log(`${a} ${b}`);
+
+// Q=6
+var c = "Open";
+var d = "Source";
+console.log(`${c} ${d}`);
+
+// Q=7
+var e = "Happy";
+var f = "Birthday";
+console.log(`${e} ${f}`);
+
+// Q=8
+var g = "Good";
+var h = "Have";
+console.log(`${g} ${h}`);
+
+// Q=9
+var i = "Web";
+var j = "Design";
+console.log(`${i} ${j}`);
+
+// Q=10
+var k = "Artificial";
+var l = "Neural";
+console.log(`${k} ${l}`);
